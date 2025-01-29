55d2d1c61db0732e13e6fdd972a06f0595acb324
Batool D Shilleh <93814390+batooldshilleh@users.noreply.github.com>
2025-01-29 21:29:41 +0200

    Create SystemUpdateMaintenance.sh

---
+++ SystemUpdateMaintenance.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+apt-get update -y # This command will update your system to last version of utilities.
+apt-get upgrade -y # This command will upgrade your system to last version of utilities.
+apt-get remove -y # This command will remove unnecessary utilities from your system.
+
+
+
